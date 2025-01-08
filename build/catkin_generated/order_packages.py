be6846cd511d942652a075dc1ce541b2f7898660
Alex <152740980+AlexHuang006@users.noreply.github.com>
2025-01-08 15:36:43 +0000

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/long/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/long/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/long/catkin_ws/devel;/opt/ros/noetic' != '' else []
